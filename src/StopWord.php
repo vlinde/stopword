8deb4a3252734929463f39459e08d091e3572030
Bogdan Miret <miret.bogdan@vlinde.com>
2020-04-13 19:47:16 +0300

    Stopword languages files

--- src/StopWord.php
+++ src/StopWord.php
@@ -2,36 +2,42 @@
 
 namespace Vlinde\StopWord;
 
-use Vlinde\StopWord\Models\Keyword;
 use Vlinde\StopWord\Helpers\Functions;
+use Vlinde\StopWord\Models\Keyword;
 
 class StopWord
 {
-    public function filter($string , $words_longer_than = 2 , $number_of_words = 4, $locale = 'en')
+    public function filter($string, $words_longer_than = 2, $number_of_words = 4, $locale = 'en')
     {
-        $char_map = config('stopword.' . $locale) ?? [];
+        $path =  __DIR__ . '/../stopwords/' . $locale . '.php';
 
-        $replace_exceptions = ['php' , 'js', 'bäckerei'];
+        if(!file_exists($path)) {
+            return 0;
+        }
 
-        $imploded_exceptions = implode('|' , $replace_exceptions);
+        $char_map = require_once $path;
 
-        $clean = str_replace(array_keys($char_map) , $char_map , " " . mb_strtolower($string) . " ");
+        $replace_exceptions = ['php', 'js', 'bäckerei'];
 
-        $clean = mb_ereg_replace("/\b[a-z|A-Z]{1," . $words_longer_than . "}\b(?<!" . $imploded_exceptions . ")/" , "  " , $clean);
+        $imploded_exceptions = implode('|', $replace_exceptions);
 
-        $clean = explode(' ' , $clean); // separate by space
+        $clean = str_replace(array_keys($char_map), $char_map, " " . mb_strtolower($string) . " ");
+
+        $clean = mb_ereg_replace("/\b[a-z|A-Z]{1," . $words_longer_than . "}\b(?<!" . $imploded_exceptions . ")/", "  ", $clean);
+
+        $clean = explode(' ', $clean); // separate by space
 
         $clean = array_filter($clean);  // remove empty values from array
 
         $clean = array_unique($clean); // remove duplicates
 
-        $clean = array_map('mb_strtolower' , $clean);
+        $clean = array_map('mb_strtolower', $clean);
 
-        $clean = array_slice($clean , 0 , $number_of_words);   // limit number of results
+        $clean = array_slice($clean, 0, $number_of_words);   // limit number of results
 
         $count = 0;
 
-        if(empty($clean)) {
+        if (empty($clean)) {
             return $count;
         }
 
@@ -39,8 +45,8 @@ class StopWord
 
         $count = count($combinations);
 
-        foreach($combinations as $combination) {
-            if($existing_keyword = Keyword::where('key' , $combination)->first()) {
+        foreach ($combinations as $combination) {
+            if ($existing_keyword = Keyword::where('key', $combination)->first()) {
                 $existing_keyword->counter += 1;
 
                 $existing_keyword->withoutSyncingToSearch(function () use ($existing_keyword) {
