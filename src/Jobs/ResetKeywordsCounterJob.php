2872586424d3967cae565c063f71aab777b985ec
Bogdan Miret <miret.bogdan@vlinde.com>
2020-04-15 13:36:54 +0300

    Job to reset the keywords counter

---
+++ src/Jobs/ResetKeywordsCounterJob.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Vlinde\StopWord\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Vlinde\StopWord\Models\Keyword;
+
+class ResetKeywordsCounterJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $skip;
+    public $take;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $skip
+     * @param $take
+     */
+    public function __construct($skip, $take)
+    {
+        $this->skip = $skip;
+        $this->take = $take;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $keywords = Keyword::skip($this->skip)
+            ->take($this->take)
+            ->get();
+
+        if ($keywords->isEmpty()) {
+            return;
+        }
+
+        $keywords->each(function ($keyword) {
+            $keyword->counter = 0;
+
+            $keyword->withoutSyncingToSearch(function () use ($keyword) {
+                $keyword->save();
+            });
+
+        });
+    }
+}
