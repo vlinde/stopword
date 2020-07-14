cb008f626cb77298c956f0084b3935c70b8b3d6f
Bogdan Miret <miret.bogdan@vlinde.com>
2020-07-14 14:53:14 +0300

    Stop word creating keywords improvements

--- src/StopWord.php
+++ src/StopWord.php
@@ -7,11 +7,45 @@ use Vlinde\StopWord\Models\Keyword;
 
 class StopWord
 {
-    protected $defaultLocale = 'en';
+    const DEFAULT_LOCALE = 'en';
 
-    public function filter($string, $words_longer_than = 2, $number_of_words = 4, $locale = '')
+    public function createKeywords(
+        string $string,
+        int $wordsLongerThan = 2,
+        int $numberOfWords = 4,
+        string $locale = null
+    ): int
     {
-        $stopWordLocale = !empty($locale) ? $locale : $this->defaultLocale;
+        $combinations = $this->generateCleanCombinations($string, $wordsLongerThan, $numberOfWords, $locale);
+
+        $count = 0;
+
+        if (empty($combinations)) {
+            return $count;
+        }
+
+        $keywords = Keyword::where(function ($query) use ($combinations) {
+            foreach ($combinations as $combination) {
+                $query->orWhere('key', '=', $combination);
+            }
+        })->get();
+
+        foreach ($combinations as $combination) {
+            $keyword = $keywords->where('key', '=', $combination);
+
+            if ($keyword->isEmpty()) {
+                $this->generateNewKeyword($combination, $locale);
+            } else {
+                $this->increaseKeywordCounter($keyword);
+            }
+        }
+
+        return $count;
+    }
+
+    protected function generateCleanCombinations(string $string, $wordsLongerThan = 2, $numberOfWords = 4, string $locale = null)
+    {
+        $stopWordLocale = $locale ?? self::DEFAULT_LOCALE;
 
         $path = __DIR__ . '/../stopwords/' . $stopWordLocale . '.php';
 
@@ -19,25 +53,62 @@ class StopWord
             return 0;
         }
 
-        $char_map = require $path;
+        $charMap = require $path;
 
-        $replace_exceptions = ['php', 'js', 'bäckerei'];
+        $replaceExceptions = ['php', 'js', 'bäckerei'];
 
-        $imploded_exceptions = implode('|', $replace_exceptions);
+        $implodedExceptions = implode('|', $replaceExceptions);
 
-        $clean = str_replace(array_keys($char_map), $char_map, " " . mb_strtolower($string) . " ");
+        $clean = str_replace(array_keys($charMap), $charMap, " " . mb_strtolower($string) . " ");
 
-        $clean = mb_ereg_replace("/\b[a-z|A-Z]{1," . $words_longer_than . "}\b(?<!" . $imploded_exceptions . ")/", "  ", $clean);
+        $clean = mb_ereg_replace("/\b[a-z|A-Z]{1," . $wordsLongerThan . "}\b(?<!" . $implodedExceptions . ")/", "  ", $clean);
 
-        $clean = explode(' ', $clean); // separate by space
+        $clean = explode(' ', $clean);
 
-        $clean = array_filter($clean);  // remove empty values from array
+        $clean = array_filter($clean);
 
-        $clean = array_unique($clean); // remove duplicates
-
+        $clean = array_unique($clean);
         $clean = array_map('mb_strtolower', $clean);
 
-        $clean = array_slice($clean, 0, $number_of_words);   // limit number of results
+        return array_slice($clean, 0, $numberOfWords);
+    }
+
+    protected function generateNewKeyword(string $combination, string $locale, bool $sync = false): Keyword
+    {
+        $keyword = new Keyword();
+        $keyword->key = $combination;
+        $keyword->locale = $locale;
+
+        if ($sync) {
+            return tap($keyword)->save();
+        }
+
+        return $keyword->withoutSyncingToSearch(function () use ($keyword) {
+            return tap($keyword)->save();
+        });
+    }
+
+    protected function increaseKeywordCounter(Keyword $keyword, bool $sync = false): Keyword
+    {
+        $keyword->counter += 1;
+
+        if ($sync) {
+            return tap($keyword)->save();
+        }
+
+        return $keyword->withoutSyncingToSearch(function () use ($keyword) {
+            return tap($keyword)->save();
+        });
+    }
+
+    public function createKeywordsWithCombinations(
+        string $string,
+        int $wordsLongerThan = 2,
+        int $numberOfWords = 4,
+        string $locale = null
+    ): int
+    {
+        $this->cleanWithStopwords($string, $wordsLongerThan, $numberOfWords, $locale);
 
         $count = 0;
 
@@ -49,8 +120,8 @@ class StopWord
 
         $count = count($combinations);
 
-        $existing_keywords_all = Keyword::where(function($query) use($combinations) {
-            foreach($combinations as $combination) {
+        $existing_keywords_all = Keyword::where(function ($query) use ($combinations) {
+            foreach ($combinations as $combination) {
                 $query->orWhere('key', '=', $combination);
             }
         })->get();
@@ -59,52 +130,32 @@ class StopWord
 
             $existing_keywords = $existing_keywords_all->where('key', '=', $combination);
 
-            if($existing_keywords->isNotEmpty()) {
+            if ($existing_keywords->isNotEmpty()) {
                 $empty_locale_keyword = $existing_keywords->where('locale', '=', '');
 
-                if($empty_locale_keyword->isNotEmpty()) {
+                if ($empty_locale_keyword->isNotEmpty()) {
                     $existing_keyword = $empty_locale_keyword->first();
 
-                    $existing_keyword->counter += 1;
-                    $existing_keyword->withoutSyncingToSearch(function () use ($existing_keyword) {
-                        $existing_keyword->save();
-                    });
+                    $this->increaseKeywordCounter($existing_keyword);
 
                     continue;
                 }
 
                 $exists_locale_keyword = $existing_keywords->where('locale', '=', $locale);
 
-                if($exists_locale_keyword->isEmpty()) {
+                if ($exists_locale_keyword->isEmpty()) {
 
-                    $new_keyword = new Keyword();
-                    $new_keyword->key = $combination;
-                    $new_keyword->locale = $locale;
-
-                    $new_keyword->withoutSyncingToSearch(function () use ($new_keyword) {
-                        $new_keyword->save();
-                    });
+                    $this->generateNewKeyword($combination, $locale);
                 } else {
                     $existing_keyword = $exists_locale_keyword->first();
 
-                    $existing_keyword->counter += 1;
-
-                    $existing_keyword->withoutSyncingToSearch(function () use ($existing_keyword) {
-                        $existing_keyword->save();
-                    });
+                    $this->increaseKeywordCounter($existing_keyword);
                 }
             } else {
-                $new_keyword = new Keyword();
-                $new_keyword->key = $combination;
-                $new_keyword->locale = $locale;
-
-                $new_keyword->withoutSyncingToSearch(function () use ($new_keyword) {
-                    $new_keyword->save();
-                });
+                $this->generateNewKeyword($combination, $locale);
             }
         }
 
         return $count;
-
     }
 }
